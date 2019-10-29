ad27cc9fb76ab04fe7de5f8a8b0c9807e5062b74
Scott Hurrey <Scott.Hurrey@blackboard.com>
2019-10-29 16:06:06 -0400

    Updated to allow for more command-line configuration

--- getContents.py
+++ getContents.py
@@ -3,19 +3,25 @@ import os
 import requests
 
 parser = argparse.ArgumentParser(description="This script allows you to pull all specified content types from Jive with the specified tags")
-parser.add_argument("--u", type=str, required=True, help="Your Jive username *required")
-parser.add_argument("--p", type=str, required=True, help="Your Jive password *required")
-parser.add_argument("--t", type=str, default="developer,developers", help="An optional comma-delimited list of tags, i.e. -t developer,developers <- This is the default")
-parser.add_argument("--d", type=str, default="document,post,video,idea", help="An option comma-delimited list of document types, i.e. -d document,post,video,idea <- This is the default. This also accepts discussions, poll, and file. A post is a blog")
+parser.add_argument("-u", "--user", type=str, required=True, help="Your Jive username *required")
+parser.add_argument("-p", "--password", type=str, required=True, help="Your Jive password *required")
+parser.add_argument("-t", "--tags", type=str, default="developer,developers", help="An optional comma-delimited list of tags, i.e. --t developer,developers <- This is the default")
+parser.add_argument("-d", "--doctypes", type=str, default="document,post,video,idea", help="An optional comma-delimited list of document types, i.e. --d document,post,video,idea <- This is the default. This also accepts discussions, poll, and file. A post is a blog")
+parser.add_argument("-f", "--fields", type=str, default="content.text,subject,categories", help="An optional comma-delimited list of fields to include, i.e. --f content.text,subject,categories <- This is the default.")
+parser.add_argument("-i", "--includeblogs", action="store_true", default=True, help="Flag to determine whether to include blogs. Default is True")
+parser.add_argument("-c", "--count", type=int, default=100, help="Number of records to return. Default is 100")
 
 args = parser.parse_args()
 
-username = args.u
-password = args.p
-tags = args.t
-doctypes = args.d
+username = args.user
+password = args.password
+tags = args.tags
+doctypes = args.doctypes
+fields = args.fields
+includeBlogs = args.includeblogs
+count = args.count
 
-endpoint = 'https://community.blackboard.com/api/core/v3/contents?filter=tag(' + tags + ')&filter=type(' + doctypes + ')&fields=content.text,subject,categories&includeBlogs=true&count=100'
+endpoint = 'https://community.blackboard.com/api/core/v3/contents?filter=tag(' + tags + ')&filter=type(' + doctypes + ')&includeBlogs=' + str(includeBlogs) + '&count=' + str(count) #+ '&fields=' + fields
 print('{}'.format(endpoint))
 
 if not os.path.exists('content'):
